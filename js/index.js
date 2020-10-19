08c80a7cce4f66799fb7f6ba6553834bbe669e66
Jérémie Bernard <jeremie71000@hotmail.fr>
2020-10-19 17:08:19 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
 new Vue({
-	el: '#app',
-	data: {
-		size: null,
-		speed: null,
-		result: null
-	},
-	methods: {
-		calculate() {
-			if (!this.size || this.size == 0 || !this.speed) {
-				this.result = null;
-				return;
-			}
-			let result = this.size * 1000;
-			result /= this.speed;
-			result /= 3600;
-			if (isNaN(result)) {
-				return;
-			}
-			this.result = result.toFixed(2);
-		}
-	}
+  el: '#app',
+  data: {
+    size: null,
+    speed: null,
+    result: null
+  },
+  methods: {
+    calculate() {
+      if (!this.size || this.size == 0 || !this.speed) {
+        this.result = null;
+        return;
+      }
+      let result = this.size * 1000;
+      result /= this.speed;
+      result /= 3600;
+      if (isNaN(result)) {
+        return;
+      }
+      this.result = result.toFixed(2);
+    }
+  }
 });
